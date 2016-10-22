90abdb7e62f48d6c88a2d8c63f46b4eb6b48198d
roman <roman_991@bk.ru>
2016-10-22 16:35:32 +0300

    Контакты, футер, карта

--- app/Http/Controllers/FrontEndController.php
+++ app/Http/Controllers/FrontEndController.php
@@ -6,6 +6,7 @@ use App\Contact;
 use App\FeedBack;
 use App\ImageStorage;
 use App\Slider;
+use App\Work;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
@@ -13,6 +14,7 @@ use App\Http\Requests;
 class FrontEndController extends Controller
 {
     public function index () {
+        date_default_timezone_set('UTC');
         $contact = Contact::where('status', 1)->first();
         $contacts = [];
         if ($contact) {
@@ -38,10 +40,21 @@ class FrontEndController extends Controller
             $feedback->avatar = $IM->getCropped('avatar', 102, 102)[0];
         }
 
+        $works = Work::where('status', 1)->get();
+        foreach ($works as &$work) {
+            $IM = new ImageStorage($work);
+            $work->miniSrc = $IM->getCropped('image', 340, 224);
+            $work->origSrc = $IM->getOrigImage('image');
+        }
+
+        $dateYear = date('Y');
+
         return view('index', [
             'contacts' => $contacts,
             'slides' => $slides,
             'feedbacks' => $feedbacks,
+            'works' => $works,
+            'dateYear' => $dateYear,
         ]);
     }
 }
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -213,5 +213,206 @@
             </div>
         </div>
         @endif
+        @if(isset($works))
+            <div class='gallery-block'>
+                <div class='gallery'>
+                    @foreach($works as $item)
+                    <div class='gal-item'>
+                        <a class="example-image-link" href="{{ $item->origSrc[0] }}" data-lightbox="example-set" data-title="<span class='title-head'>{{ $item->name }}</span><br> <span class='title-main'>{{ $item->description }}</span>">
+                            <img class="example-image" src="{{ $item->miniSrc[0] }}" alt="{{ $item->name }}"/>
+                            <div class='gal-desc'>
+                                <div class='gal-desc-head'>
+                                    {{ $item->name }}
+                                </div>
+                                <div class='gal-desc-content'>
+                                    {{ $item->description }}
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    @endforeach
+                </div>
+            </div>
+        @endif
+        <div class='clearbox'></div>
+        <div class="contacts">
+            <div class="contacts-content">
+                <div class="contant-form">
+                    <!-- @TODO: Тут надо переделать на отправку контроллеру создания нового заказа -->
+                    <form action="#" method="post" id="cForm1">
+                        <div class="contant-head">Решились на ремонт?
+                            Свяжитесь с нами!</div>
+                        <div>
+                            <input type="text" id="posName" placeholder="Имя">
+                        </div>
+                        <div>
+                            <input type="email" id="posEmail" placeholder="Email">
+                        </div>
+                        <div>
+                            <input type="tel" id="posTheme" placeholder="Тема">
+                        </div>
+                        <div>
+                            <textarea type="text" id="posText" class="message" placeholder="Сообщение"></textarea>
+                        </div>
+                        <div>
+                            <button type="button" id="send" class="formStyle submit-button">Отправить</button>
+                        </div>
+                    </form>
+                    <div align="center" id='loadBar'></div>
+                </div>
+                <div class="contact-info">
+                    <div class="contant-head">
+                        Наши контакты
+                    </div>
+                    <div class="contact-address">
+                        {{ $contacts['address'] }}
+                    </div>
+                    <div class="contact-phone">
+                        {{ $contacts['phone'] }}
+                    </div>
+                </div>
+                <div class="clearbox"></div>
+            </div>
+        </div>
     </div>
+    <script src='https://maps.googleapis.com/maps/api/js?key=AIzaSyAM5MKlq4WL9Sidyxdb-pkvuMsD0Pug9PI&sensor=false&extension=.js'></script>
+    <script>
+        google.maps.event.addDomListener(window, 'load', init);
+        var map;
+        function init() {
+            var mapOptions = {
+                center: new google.maps.LatLng(55.759553,37.624811),
+                zoom: 14,
+                zoomControl: false,
+                disableDoubleClickZoom: true,
+                mapTypeControl: false,
+                scaleControl: false,
+                scrollwheel: true,
+                panControl: false,
+                streetViewControl: false,
+                draggable : true,
+                overviewMapControl: false,
+                overviewMapControlOptions: {
+                    opened: false,
+                },
+                mapTypeId: google.maps.MapTypeId.ROADMAP,
+                styles: [{"featureType":"all","elementType":"labels.text.fill","stylers":[{"saturation":36},{"color":"#000000"},{"lightness":40}]},{"featureType":"all","elementType":"labels.text.stroke","stylers":[{"visibility":"on"},{"color":"#000000"},{"lightness":16}]},{"featureType":"all","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#395674"},{"lightness":20}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#000000"},{"lightness":17},{"weight":1.2}]},{"featureType":"administrative","elementType":"labels.text","stylers":[{"color":"#080a0b"},{"visibility":"simplified"}]},{"featureType":"administrative","elementType":"labels.text.fill","stylers":[{"color":"#c1d4e9"},{"weight":"0.95"}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":20}]},{"featureType":"landscape","elementType":"geometry.fill","stylers":[{"color":"#415f7f"}]},{"featureType":"landscape","elementType":"labels.text","stylers":[{"hue":"#ff0000"}]},{"featureType":"landscape","elementType":"labels.text.fill","stylers":[{"color":"#c1d4e9"}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":21}]},{"featureType":"poi","elementType":"geometry.fill","stylers":[{"color":"#395674"}]},{"featureType":"poi","elementType":"labels.text.fill","stylers":[{"color":"#c1d4e9"}]},{"featureType":"road","elementType":"labels.text.fill","stylers":[{"color":"#c1d4e9"},{"weight":"1"}]},{"featureType":"road","elementType":"labels.text.stroke","stylers":[{"visibility":"off"}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#456485"},{"lightness":17}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#000000"},{"lightness":29},{"weight":0.2}]},{"featureType":"road.highway","elementType":"labels.text.fill","stylers":[{"color":"#c1d4e9"}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":18}]},{"featureType":"road.arterial","elementType":"geometry.fill","stylers":[{"color":"#456485"}]},{"featureType":"road.arterial","elementType":"labels.text.fill","stylers":[{"color":"#c1d4e9"}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":16}]},{"featureType":"road.local","elementType":"geometry.fill","stylers":[{"color":"#456485"}]},{"featureType":"road.local","elementType":"labels.text.fill","stylers":[{"color":"#c1d4e9"}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":19},{"visibility":"off"}]},{"featureType":"transit","elementType":"labels.text","stylers":[{"color":"#c1d4e9"},{"visibility":"off"}]},{"featureType":"water","elementType":"geometry","stylers":[{"color":"#9ab3cc"},{"lightness":17}]},{"featureType":"water","elementType":"geometry.fill","stylers":[{"color":"#395674"}]},{"featureType":"water","elementType":"labels.text.fill","stylers":[{"color":"#b9cfe7"}]}],
+            }
+            var mapElement = document.getElementById('kvadrat');
+            var map = new google.maps.Map(mapElement, mapOptions);
+            var locations = [
+                ['kvadrat', 'undefined', 'undefined', 'undefined', 'undefined', 55.7592267, 37.622211600000014, 'https://mapbuildr.com/assets/img/markers/default.png']
+            ];
+            for (i = 0; i < locations.length; i++) {
+                if (locations[i][1] =='undefined'){ description ='';} else { description = locations[i][1];}
+                if (locations[i][2] =='undefined'){ telephone ='';} else { telephone = locations[i][2];}
+                if (locations[i][3] =='undefined'){ email ='';} else { email = locations[i][3];}
+                if (locations[i][4] =='undefined'){ web ='';} else { web = locations[i][4];}
+                if (locations[i][7] =='undefined'){ markericon ='';} else { markericon = locations[i][7];}
+                marker = new google.maps.Marker({
+                    icon: markericon,
+                    position: new google.maps.LatLng(locations[i][5], locations[i][6]),
+                    map: map,
+                    title: locations[i][0],
+                    desc: description,
+                    tel: telephone,
+                    email: email,
+                    web: web
+                });
+                link = '';     }
+
+        }
+    </script>
+    <style>
+        #kvadrat {
+            height:244px;
+            width:100%;
+        }
+        .gm-style-iw * {
+            display: block;
+            width: 100%;
+        }
+        .gm-style-iw h4, .gm-style-iw p {
+            margin: 0;
+            padding: 0;
+        }
+        .gm-style-iw a {
+            color: #4272db;
+        }
+    </style>
+    <div id='kvadrat'></div>
+    <footer>
+        <div class="logo">
+            <a href="/">
+                <img src="/images/white-logo.png" alt="">
+                <span>copyright {{$dateYear}}</span>
+            </a>
+        </div>
+        <div class="info">
+            <div class="info-email">
+                <a href="mailto:{{$contacts['email']}}"><img src="/images/_0011_icon-mail.png" alt="" class="icon">{{$contacts['email']}}</a>
+            </div>
+            <div class="info-phone">
+                <a href="tel:{{$contacts['phoneToLink']}}"><img src="/images/_0012_icon-phone.png" alt="" class="icon">{{$contacts['phone']}}</a>
+            </div>
+            <div class="socials">
+                <a href="{{$contacts['facebook_link']}}"><img src="/images/_0008_icon-fb-footer.png" class='fb_2' alt="facebook"></a>
+            </div>
+            <div class="socials">
+                <a href="{{$contacts['instagram_link']}}"><img src="/images/_0007_icon-inst-footer.png" class='in_2' alt="instagram"></a>
+            </div>
+        </div>
+    </footer>
+    <script src="/js/lightbox-plus-jquery.min.js"></script>
+    <script>
+        $(".fb_1").on({
+            "mouseover" : function() {
+                this.src = '/images/_0006_icon-fb-hover.png';
+            },
+            "mouseout" : function() {
+                this.src='/images/_0010_icon-fb.png';
+            }
+        });
+        $(".in_1").on({
+            "mouseover" : function() {
+                this.src = '/images/_0005_icon-inst-hover.png';
+            },
+            "mouseout" : function() {
+                this.src='/images/_0009_icon-inst.png';
+            }
+        });
+        $(".fb_2").on({
+            "mouseover" : function() {
+                this.src = '/images/_0010_icon-fb.png';
+            },
+            "mouseout" : function() {
+                this.src='/images/_0008_icon-fb-footer.png';
+            }
+        });
+        $(".in_2").on({
+            "mouseover" : function() {
+                this.src = '/images/_0009_icon-inst.png';
+            },
+            "mouseout" : function() {
+                this.src='/images/_0007_icon-inst-footer.png';
+            }
+        });
+    </script>
+    <script src="js/swiper.min.js"></script>
+    <!-- Initialize Swiper -->
+    <script>
+        var swiper = new Swiper('.swiper-container', {
+            pagination: '.swiper-pagination',
+            paginationClickable: true,
+            autoplay: 10000,
+            autoplayDisableOnInteraction: false
+        });
+
+        var swiper = new Swiper('.swiper-container2', {
+            pagination: '.swiper-pagination2',
+            nextButton: '.swiper-button-next',
+            prevButton: '.swiper-button-prev',
+            paginationClickable: true,
+        });
+    </script>
 @endsection
