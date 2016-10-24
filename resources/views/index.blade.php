a180302d605e35a235f988f8955218bc5f6fedc7
roman <roman_991@bk.ru>
2016-10-24 23:55:08 +0300

    правки в вывод

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -20,7 +20,7 @@
                 </div>
                 @if (isset($contacts['facebook_link']) && $contacts['facebook_link'])
                 <div class="socials">
-                    <a href="{{ $contacts['facebook_link'] }}" target="_blank"><img src="/images/_0010_icon-fb.png" class='fb_1' alt="facebook"></a>
+                    <a href="{{ isset($contacts['facebook_link']) ? $contacts['facebook_link'] : '' }}" target="_blank"><img src="/images/_0010_icon-fb.png" class='fb_1' alt="facebook"></a>
                 </div>
                 @endif
                 @if (isset($contacts['instagram_link']) && $contacts['instagram_link'])
@@ -412,16 +412,16 @@
         </div>
         <div class="info">
             <div class="info-email">
-                <a href="mailto:{{$contacts['email']}}"><img src="/images/_0011_icon-mail.png" alt="" class="icon">{{$contacts['email']}}</a>
+                <a href="mailto:{{isset($contacts['email']) ? $contacts['email'] : '' }}"><img src="/images/_0011_icon-mail.png" alt="" class="icon">{{isset($contacts['email']) ? $contacts['email'] : ''}}</a>
             </div>
             <div class="info-phone">
-                <a href="tel:{{$contacts['phoneToLink']}}"><img src="/images/_0012_icon-phone.png" alt="" class="icon">{{$contacts['phone']}}</a>
+                <a href="tel:{{isset($contacts['phoneToLink']) ? $contacts['phoneToLink'] : ''}}"><img src="/images/_0012_icon-phone.png" alt="" class="icon">{{isset($contacts['phone']) ? $contacts['phone'] : ''}}</a>
             </div>
             <div class="socials">
-                <a href="{{$contacts['facebook_link']}}"><img src="/images/_0008_icon-fb-footer.png" class='fb_2' alt="facebook"></a>
+                <a href="{{isset($contacts['facebook_link']) ? $contacts['facebook_link'] : ''}}"><img src="/images/_0008_icon-fb-footer.png" class='fb_2' alt="facebook"></a>
             </div>
             <div class="socials">
-                <a href="{{$contacts['instagram_link']}}"><img src="/images/_0007_icon-inst-footer.png" class='in_2' alt="instagram"></a>
+                <a href="{{isset($contacts['instagram_link']) ? $contacts['instagram_link'] : ''}}"><img src="/images/_0007_icon-inst-footer.png" class='in_2' alt="instagram"></a>
             </div>
         </div>
     </footer>
