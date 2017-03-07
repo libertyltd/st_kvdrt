200657f75abef80f857085281039d8b1435abe95
roman <roman_991@bk.ru>
2017-03-08 01:17:14 +0300

    Добавили первые элементы раздела Блог

--- resources/views/frontend/blog/list.blade.php
+++ resources/views/frontend/blog/list.blade.php
@@ -2,4 +2,11 @@
 @section('content')
     @include('frontend.fragments.first_step_form')
     @include('frontend.fragments.header')
+    <div class="container">
+        <div class="blog_list">
+            <div class="blog_list-item">
+
+            </div>
+        </div>
+    </div>
 @endsection
